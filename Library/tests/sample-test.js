c20525899e6218c0df4acbb680814ea0b4c70c82
valentinmnaydenov <85516432+valentinmnaydenov@users.noreply.github.com>
2022-07-15 11:26:42 +0300

    Update sample-test.js

--- Library/tests/sample-test.js
+++ Library/tests/sample-test.js
@@ -1,15 +1,14 @@
-const { expect, assert } = require("chai");
-const { ethers } = require("hardhat")
+ const { expect, assert } = require("chai");
+const { ethers } = require("hardhat");
 
 describe("first", function () {
-  it("Deploy", async function(){
+  it("Deploy", async function () {
     const Library = await ethers.getContractFactory("Library");
     const library = await Library.deploy();
     await library.deployed();
-
   });
 
-  it("Get owner addresses", async function(){
+  it("Get owner addresses", async function () {
     const Library = await ethers.getContractFactory("Library");
     const library = await Library.deploy();
     await library.deployed();
@@ -17,89 +16,128 @@ describe("first", function () {
     const [_owner, addr1, addr2] = await ethers.getSigners();
     const owner = await library.owner();
     await owner;
-   
   });
-describe("Add book", function () {
-  it("AddBook once and check", async function(){
-    const Library = await ethers.getContractFactory("Library");
-    const library = await Library.deploy();
-    await library.deployed();
+  describe("Add book", function () {
+    it("AddBook once and check", async function () {
+      const Library = await ethers.getContractFactory("Library");
+      const library = await Library.deploy();
+      await library.deployed();
 
-    const AddBook = await library.AddBook("2000",3);
-    await AddBook.wait();
-  });
-  
+      const AddBook = await library.AddBook("The godfather", 2);
+      await AddBook.wait();
+    });
 
-    it("Non owner try to add book",async function(){
+    it("Non owner try to add book", async function () {
       const [_owner, addr1, addr2] = await ethers.getSigners();
       const Library = await ethers.getContractFactory("Library");
       const library = await Library.deploy();
       await library.deployed();
-  
-      const addBook = await library.connect(_owner).AddBook("2000",3);
+
+      const addBook = await library.connect(_owner).AddBook("The Godfather", 2);
       await addBook.wait();
-      var err= false;
-      try{
-        const addSecondBook = await library.connect(addr1).AddBook("THE GODFATHER ",3);
+      var err = false;
+      try {
+        const addSecondBook = await library
+          .connect(addr1)
+          .AddBook("THE GODFATHER ", 2);
         await addSecondBook.wait();
-      }catch(error){
-        err=true;
+      } catch (error) {
+        err = true;
         expect(error.name).to.equals("Error");
-      };
+      }
     });
   });
-    describe("Borrow book", function () {
-      it("Should borrow a book", async function(){
-        const Library = await ethers.getContractFactory("Library");
-        const library = await Library.deploy();
-        await library.deployed();
-    
-        const AddBook = await library.AddBook("2000",3);
-        await AddBook.wait();
-    
-        const borrowBook = await library.borrowBook(3);
-        await borrowBook.wait();
-    
-      }); 
+  describe("Borrow book", function () {
+    it("Should borrow a book", async function () {
+      const Library = await ethers.getContractFactory("Library");
+      const library = await Library.deploy();
+      await library.deployed();
 
-      it("BorrowBook for second borrowing", async function(){
-        const Library = await ethers.getContractFactory("Library");
-        const library = await Library.deploy();
-        await library.deployed();
-    
-        const addBook = await library.AddBook("2000",3);
-        await addBook.wait();
-    
-        const borrowBook = await library.borrowBook(3);
-        await borrowBook.wait();
-        var err=false;
-        try {
-          await library.borrowBook(3);
-        } catch(error) {
-          err=true;
-          expect(error.name).to.equals("Error");
-        };
-        expect(err).to.equal(true);
-      });
+      const AddBook = await library.AddBook("The Godfather", 2);
+      await AddBook.wait();
 
+      const borrowBook = await library.borrowBook(
+        ethers.utils.solidityKeccak256(
+          ["bytes"],
+          [ethers.utils.solidityPack(["string"], ["The Godfather"])]
+        )
+      );
+      await borrowBook.wait();
+    });
+
+    it("BorrowBook for second borrowing", async function () {
+      const Library = await ethers.getContractFactory("Library");
+      const library = await Library.deploy();
+      await library.deployed();
+
+      const addBook = await library.AddBook("The Godfather", 2);
+      await addBook.wait();
+
+      const borrowBook = await library.borrowBook(
+        ethers.utils.solidityKeccak256(
+          ["bytes"],
+          [ethers.utils.solidityPack(["string"], ["The Godfather"])]
+        )
+      );
+      await borrowBook.wait();
+      var err = false;
+      try {
+        await library.borrowBook(3);
+      } catch (error) {
+        err = true;
+        expect(error.name).to.equals("Error");
+      }
+      expect(err).to.equal(true);
+    });
 
     describe("Return a book", function () {
-           it("Should return a book", async function(){
-             const Library = await ethers.getContractFactory("Library");
-             const library = await Library.deploy();
-             await library.deployed();
-              
-             const AddBook = await library.AddBook("2000",3);
-             await AddBook.wait();
-             const borrowBook = await library.borrowBook(3);
-             await borrowBook.wait();   
-              
-             const returnCurrentBook = await library. returnCurrentBook(3);
-             await returnCurrentBook.wait();
-             
+      it("Should return a book", async function () {
+        const Library = await ethers.getContractFactory("Library");
+        const library = await Library.deploy();
+        await library.deployed();
+
+        const AddBook = await library.AddBook("The Godfather", 2);
+        await AddBook.wait();
+        const borrowBook = await library.borrowBook(
+          ethers.utils.solidityKeccak256(
+            ["bytes"],
+            [ethers.utils.solidityPack(["string"], ["The Godfather"])]
+          )
+        );
+        await borrowBook.wait();
+
+        const returnCurrentBook = await library.borrowBook(
+          ethers.utils.solidityKeccak256(
+            ["bytes"],
+            [ethers.utils.solidityPack(["string"], ["The Godfather"])]
+          )
+        );
+        await returnCurrentBook.wait();
       });
-        
+
+      it(" Should NOT be able to return book if it was not taken", async function () {
+        const Library = await ethers.getContractFactory("Library");
+        const library = await Library.deploy();
+        await library.deployed();
+
+        const AddBook = await library.AddBook("The Godfather", 2);
+        await AddBook.wait();
+
+        await library.borrowBook(
+          ethers.utils.solidityKeccak256(
+            ["bytes"],
+            [ethers.utils.solidityPack(["string"], ["The Godfather"])]
+          )
+        );
+        await expect(
+          library.returnCurrentBook(
+            ethers.utils.solidityKeccak256(
+              ["bytes"],
+              [ethers.utils.solidityPack(["string"], ["The Godfather"])]
+            )
+          )
+        );
       });
- 
-    })
     });
+  });
+});
